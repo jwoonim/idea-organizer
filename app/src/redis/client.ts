76c4010730e63db408d655cfa0631cc15342c731
jwoonim <jwoonim90@gmail.com>
2021-06-25 19:56:10 +0900

    build: change redis client

--- app/src/redis/client.ts
+++ app/src/redis/client.ts
@@ -1,11 +1,11 @@
-import Redis from 'ioredis'
+import redis from 'redis'
 
 import { Secrets } from 'src/utils/constants'
 
-export const redisClient = new Redis(
+export const redisClient = redis.createClient(
   Number(Secrets.REDIS_PORT),
   Secrets.REDIS_HOST,
-  { retryStrategy: () => 1000 }
+  { retry_strategy: () => 1000 }
 )
 
 // Promisify the get function.
