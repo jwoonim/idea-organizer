9331f6af1733b3b5023590d1361aca0c791c5fd7
jwoonim <jwoonim90@gmail.com>
2021-07-03 21:07:52 +0900

    fix: sending an email with nodemailer

--- app/src/utils/email/send-email.ts
+++ app/src/utils/email/send-email.ts
@@ -1,4 +1,3 @@
-import { google } from 'googleapis'
 import nodemailer from 'nodemailer'
 
 import { Secrets, EmailType, EmailTypes } from 'src/utils/constants'
@@ -7,12 +6,13 @@ import {
   getResetPasswordEmailTemplate,
 } from './templates'
 
-const OAuth2 = google.auth.OAuth2
-const oauth2Client = new OAuth2(
-  Secrets.GOOGLE_CLIENT_ID,
-  Secrets.GOOGLE_CLIENT_SECRET,
-  'https://developers.google.com/oauthplayground'
-)
+const smtpTransport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: Secrets.GMAIL_ACCOUNT,
+    pass: Secrets.GMAIL_ACCOUNT_APP_PASS,
+  },
+})
 
 export const sendEmail = async (
   to: string,
@@ -20,38 +20,26 @@ export const sendEmail = async (
   name: string,
   emailType: EmailType
 ): Promise<any> => {
-  // Set refresh token and get access token
-  oauth2Client.setCredentials({ refresh_token: Secrets.OAUTH_REFRESH_TOKEN })
-  const accessToken = await oauth2Client.getAccessToken()
+  try {
+    let subject, template
+    if (emailType === EmailTypes.VERIFY_EMAIL) {
+      subject = 'IO - Email verification'
+      template = getEmailVerificationEmailTemplate(name, url)
+    } else if (emailType === EmailTypes.RESET_PASSWORD) {
+      subject = 'IO - Reset password'
+      template = getResetPasswordEmailTemplate(name, url)
+    }
 
-  const smtpTransport = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      type: 'OAuth2',
-      user: Secrets.GMAIL_ACCOUNT,
-      clientId: Secrets.GOOGLE_CLIENT_ID,
-      clientSecret: Secrets.GOOGLE_CLIENT_SECRET,
-      refreshToken: Secrets.OAUTH_REFRESH_TOKEN,
-      accessToken: accessToken,
-    },
-  })
+    const mailOptions = {
+      from: Secrets.GMAIL_ACCOUNT,
+      to: to,
+      subject: subject,
+      generateTextFromHTML: true,
+      html: template,
+    }
 
-  let subject, template
-  if (emailType === EmailTypes.VERIFY_EMAIL) {
-    subject = 'IO - Email verification'
-    template = getEmailVerificationEmailTemplate(name, url)
-  } else if (emailType === EmailTypes.RESET_PASSWORD) {
-    subject = 'IO - Reset password'
-    template = getResetPasswordEmailTemplate(name, url)
+    return await smtpTransport.sendMail(mailOptions)
+  } catch (error) {
+    console.log(error)
   }
-
-  const mailOptions = {
-    from: Secrets.GMAIL_ACCOUNT,
-    to: to,
-    subject: subject,
-    generateTextFromHTML: true,
-    html: template,
-  }
-
-  return await smtpTransport.sendMail(mailOptions)
 }
